083bbdbb73d57951d3c22f8ddeb5b3ee0bc5a65a
Alex Rogers <arogers@datumtechnology.com>
2022-10-30 02:20:42 +0000

    Adjusted command extraction Added discord start command

--- BukitLib/Discord/BukitDiscord.cs
+++ BukitLib/Discord/BukitDiscord.cs
@@ -1,5 +1,8 @@
 ﻿using BukitLib.Discord.Commands;
 using BukitLib.Exceptions;
+using BukitLib.Exceptions.Discord;
+using Discord;
+using Discord.WebSocket;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -12,10 +15,13 @@ namespace BukitLib.Discord
     internal static Assembly? ProjAssembly = null;
     
     /// <summary>List of discord commands</summary>
-    internal static List<BukitCommand> CommandList;
+    internal static List<BukitCommand>? CommandList = null;
 
     /// <summary>Discord Token</summary>
     private string Token;
+
+    /// <summary>Discord client</summary>
+    private DiscordSocketClient Client;
 
 
     /// <summary>
@@ -29,7 +35,67 @@ namespace BukitLib.Discord
       if(nAssembly == null)
         throw new BukitException("Assembly cannot be null!");
 
-      this.Token = nToken;
+      ProjAssembly = nAssembly;
+      Token = nToken;
+
+      RetrieveCommandList();
+    }
+
+
+    /// <summary>
+    /// Runs discord
+    /// </summary>
+    /// <returns>Thread that contains discord client</returns>
+    public async Task<Thread> Run()
+    { 
+      Thread thread = new Thread(async () => await RunDiscord().ConfigureAwait(false));
+
+      thread.Start();
+      return thread;
+    }
+
+
+    /// <summary>
+    /// Runs discord client
+    /// </summary>
+    private async Task RunDiscord()
+    { 
+      Client = new DiscordSocketClient();
+      
+      await Client.LoginAsync(TokenType.Bot, Token);
+      await Client.StartAsync();
+
+      await Task.Delay(-1);
+    }
+
+
+    /// <summary>
+    /// Retrieves and assigns command list from Assembly
+    /// </summary>
+    private void RetrieveCommandList()
+    { 
+      if(CommandList != null)
+        return;
+
+      CommandList = new List<BukitCommand>();
+
+      List<Type> typeLst = ProjAssembly!
+                            .GetTypes()
+                            .Where(x => x.IsSubclassOf(typeof(BukitCommand)))
+                            .ToList();
+
+      
+      // Creates an instance of each 
+      try
+      { 
+        for(int x = 0; x < typeLst.Count; x++)
+        { 
+          CommandList.Add((BukitCommand)Activator.CreateInstance(typeLst[x])!);
+        }
+      } catch(MissingMethodException nException)
+      { 
+        throw new BukitDiscordException($"Command constructors must be public!\n{nException.Message}");
+      }
     }
   }
 }
